3263e1ffc020fa7321893309a199f16c3683e1db
igorkorobko <nekto.korobko@gmail.com>
2021-10-15 00:10:18 +0300

    correct output data to client

--- commands/get_cost.py
+++ commands/get_cost.py
@@ -6,19 +6,20 @@ def get_cost(update, context):
     if len(text) == 1:
         update.message.reply_text("Введите ticket интересующуй вас акции")
     else:
-        ticket = text[1].lower().capitalize()
+        ticket = text[1].lower().upper()
         price = get_price(ticket)
         if price is not None:
             update.message.reply_text(
-                f'Наименование тикета: {price["ticket_name"]} \n'
-                f'Стоимость акции: {price["cost"]} \n'
-                f'Цена открытия: {price["cost_open"]} \n'
-                f'Цена закрытия: {price["cost_close"]} \n'
-                f'Максимальная стоимость за торги: {price["max_cost"]} \n'
-                f'Минимальная стоимость за торги: {price["min_cost"]} \n')
+                f'Наименование тикета: {price[0]} \n'
+                f'Стоимость акции: {(price[1])/100} \n'
+                f'Цена открытия: {price[2]/100} \n'
+                f'Цена закрытия: {price[3]/100} \n'
+                f'Максимальная стоимость за торги: {price[4]/100} \n'
+                f'Минимальная стоимость за торги: {price[5]/100} \n')
         else:
             update.message.reply_text('По вашему запросу ничего не найдено. Попробуйте изменить название акции и '
                                       'повторно сделать запрос.')
+
 
 if __name__ == '__main__':
     pass
