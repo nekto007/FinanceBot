caa8f71d1b9d91c98c2c6cb23ec44b7732eb84a6
igorkorobko <nekto.korobko@gmail.com>
2021-10-16 01:19:02 +0300

    correct function get_average

--- api/moex/price.py
+++ api/moex/price.py
@@ -2,7 +2,10 @@ import requests
 from auth import authorization
 from datetime import datetime, timedelta
 from db.db_connect import db_session
-from models.db_models import StockInfo, Dividents
+from models.db_models import StockInfo, Dividents, StockHistory, Calendar
+
+
+# import prod
 
 
 def get_price(emitet):
@@ -18,15 +21,17 @@ def get_price(emitet):
         if len(stock_data):
             count_string = db_session.query(StockInfo, StockInfo.id).filter(StockInfo.sec_id == emitet).count()
             if count_string:
-                stock_info = {'open_price': int(stock_data[9]*100), 'close_price': int(stock_data[49]*100),
-                              'current_cost': int(stock_data[12]*100), 'low_cost_daily': int(stock_data[10]*100),
-                              'high_cost_daily': int(stock_data[10]*100), 'updated_at': datetime.now()}
+                stock_info = {'open_price': int(stock_data[9] * 100), 'close_price': int(stock_data[49] * 100),
+                              'current_cost': int(stock_data[12] * 100), 'low_cost_daily': int(stock_data[10] * 100),
+                              'high_cost_daily': int(stock_data[10] * 100), 'updated_at': datetime.now()}
                 db_session.query(StockInfo).filter_by(sec_id=emitet).update(stock_info)
                 db_session.commit()
             else:
-                current_info = StockInfo(sec_id=stock_data[0], board_id='TQBR', open_price=int(stock_data[9]*100)
-                                         , close_price=int(stock_data[49]*100), current_cost=int(stock_data[12]*100),
-                                         low_cost_daily=int(stock_data[10]*100), high_cost_daily=int(stock_data[10]*100)
+                current_info = StockInfo(sec_id=stock_data[0], board_id='TQBR', open_price=int(stock_data[9] * 100)
+                                         , close_price=int(stock_data[49] * 100),
+                                         current_cost=int(stock_data[12] * 100),
+                                         low_cost_daily=int(stock_data[10] * 100),
+                                         high_cost_daily=int(stock_data[10] * 100)
                                          )
                 db_session.add(current_info)
                 db_session.commit()
@@ -41,20 +46,52 @@ def get_price(emitet):
 def get_average(emitet, days):
     history_price = []
     average = {}
+    trade_list_dates = []
     # authorization.is_cookie_expired(authorization.get_auth())  # Проверка текущего куки на валидность
-    date_ago = (datetime.now() - timedelta(days)).strftime("%Y-%m-%d")
-    yesterday = (datetime.now() - timedelta(1)).strftime("%Y-%m-%d")
-    url = f'https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/{emitet}' \
-          f'.json?from={date_ago}&till={yesterday} '
-    response = requests.get(url, cookies=authorization.get_auth()).json()
-    history_data = response['history']['data']
-    if len(history_data) != 0:
-        for element in history_data:
-            history_price.append(element[11])
-        average['ticket_name'] = history_data[0][3]
-        average['company_name'] = history_data[0][2]
-        average[f'average'] = round(sum(history_price) / len(history_price), 3)
-        return average
+    date_ago = (datetime.now() - timedelta(days)).date()
+    yesterday = (datetime.now() - timedelta(1)).date()
+    working_date = db_session.query(Calendar.date).filter(Calendar.date >= str(date_ago),
+                                                          Calendar.date <= str(yesterday)).all()
+    trade_dates = db_session.query(StockHistory.trade_date).filter(StockHistory.sec_id == emitet,
+                                                                   StockHistory.trade_date >= str(date_ago)).all()
+
+    if len(working_date) != len(trade_dates):
+        url = f'https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/{emitet}' \
+              f'.json?from={date_ago}&till={yesterday} '
+        response = requests.get(url, cookies=authorization.get_auth()).json()
+        history_data = response['history']['data']
+        if len(history_data) != 0:
+            for element in history_data:
+                if element[1] not in trade_list_dates:
+                    stock_history = StockHistory(board_id=element[0],
+                                                 trade_date=element[1],
+                                                 short_name=element[2], sec_id=element[3],
+                                                 number_of_trades=int(element[4]),
+                                                 value_trade=int(element[5]), open_cost=int(element[6] * 100),
+                                                 low_cost=int(element[7] * 100), high_cost=int(element[8] * 100),
+                                                 close_cost=int(element[9] * 100), created_at=datetime.now(),
+                                                 updated_at=datetime.now()
+                                                 )
+                    db_session.add(stock_history)
+                    try:
+                        db_session.commit()
+                    except:
+                        db_session.rollback()
+                        # ignore error
+                        pass
+        else:
+            return None
+    history_close_costs = db_session.query(StockHistory.close_cost).filter(StockHistory.sec_id == emitet,
+                                                                           StockHistory.trade_date >= str(
+                                                                               date_ago)).all()
+    for history_close_cost in history_close_costs:
+        history_price.append(history_close_cost[0] / 100)
+    average['ticket_name'] = emitet
+    average['company_name'] = db_session.query(StockHistory.short_name).filter(StockHistory.sec_id == emitet) \
+        .first()[0]
+    average['average'] = round(sum(history_price) / len(history_price), 3)
+    db_session.close()
+    return average
 
 
 def get_date_dividents(emitet):
