f10c90ade1a82b0d05e5e4bd0102fc204ce08fd7
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2021-10-17 13:57:20 +0300

    added graph to get_candles unused days been removed from a graph

--- commands/get_candles.py
+++ commands/get_candles.py
@@ -1,9 +1,6 @@
 import plotly.graph_objects as go
 from datetime import datetime
 
-# import finplot as fplt
-# import yfinance
-
 # from db.db_connect import db_session
 from models.db_models import StockHistory
 from sqlalchemy import create_engine
@@ -11,40 +8,28 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 engine = create_engine('sqlite:///../identifier.sqlite', echo=True)
 db_session = scoped_session(sessionmaker(bind=engine))
-
-stock_scores = db_session.query(StockHistory.trade_date, StockHistory.open_cost, StockHistory.high_cost, StockHistory.low_cost,
-                         StockHistory.close_cost).filter(StockHistory.sec_id == 'TATN').all()
-# dates = []
-# open_cost = []
-# high_cost = []
-# low_cost = []
-# close_cost = []
-# for stock in stock_scores:
-#     dates.append(stock[0])
-#     open_cost.append(stock[1])
-#     high_cost.append(stock[2])
-#     low_cost.append(stock[3])
-#     close_cost.append(stock[4])
-# # print(dates, open_cost, high_cost, low_cost, close_cost)
-# fig = go.Figure(data=[go.Candlestick(x=dates,
-#                 open=open_cost,
-#                 high=high_cost,
-#                 low=low_cost,
-#                 close=close_cost)])
+emitet = 'TATN'
+stock_scores = db_session.query(StockHistory.trade_date,
+                                StockHistory.open_cost,
+                                StockHistory.high_cost,
+                                StockHistory.low_cost,
+                                StockHistory.close_cost).filter(StockHistory.sec_id == 'TATN').all()
 
 stocks_dict = {}
 for row in stock_scores:
     dates, open_cost, high_cost, low_cost, close_cost = row[0], row[1], row[2], row[3], row[4]
     stocks_dict.setdefault("dates", []).append(dates)
-    stocks_dict.setdefault("open_cost", []).append(open_cost)
-    stocks_dict.setdefault("high_cost", []).append(high_cost)
-    stocks_dict.setdefault("low_cost", []).append(low_cost)
-    stocks_dict.setdefault("close_cost", []).append(close_cost)
+    stocks_dict.setdefault("open_cost", []).append(open_cost/100)
+    stocks_dict.setdefault("high_cost", []).append(high_cost/100)
+    stocks_dict.setdefault("low_cost", []).append(low_cost/100)
+    stocks_dict.setdefault("close_cost", []).append(close_cost/100)
 
-print(stocks_dict)
-
-
-#fig.show()
-
-
-
+fig = go.Figure(data=[go.Candlestick(x=stocks_dict['dates'],
+                open=stocks_dict['open_cost'],
+                high=stocks_dict['high_cost'],
+                low=stocks_dict['low_cost'],
+                close=stocks_dict['close_cost'])],
+                layout=go.Layout(title=go.layout.Title(text=f'График движения цены эмитента {emitet}')))
+fig.update_layout(xaxis_rangeslider_visible=False, xaxis=dict(type = 'category'))
+fig.write_image("images/fig1.jpeg")
+go.Figure()
