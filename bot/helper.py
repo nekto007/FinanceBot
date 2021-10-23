73c05e7336c194e131fbe67c1a8e27049e850a62
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2021-10-23 17:05:35 +0300

    added /curr

--- bot/helper.py
+++ bot/helper.py
@@ -3,10 +3,12 @@
 def help(update, context):
     update.message.reply_text(f'/div *ticket* - информация о дивидендах: /div aflt \n'
                               f'/trand *ticket* - тренд эмитента: /trand aflt \n'
-                              f'/price *ticket* - текущая цена бумаги эмитента: /price aflt \n'
-                              f'/average15 *ticket* - скользящая средняя за 15 дней: /average15 aflt \n'
-                              f'/average50 *ticket* - скользящая средняя за 50 дней: /average50 aflt \n'
-                              f'/info - информация о торгуемых бумагах биржи: /info')
+                              f'/price *ticket*, *ticket - текущая цена бумаги эмитента: /price aflt \n'
+                              f'/avg15 *ticket*, *ticket - скользящая средняя за 15 дней: /average15 aflt \n'
+                              f'/avg50 *ticket*, *ticket - скользящая средняя за 50 дней: /average50 aflt \n'
+                              f'/curr - текущая котировка валют: /curr \n'
+                              f'/info - информация о торгуемых бумагах биржи: /info \n'
+                              f'/info *ticket* - информация о конкретной бумаге: /info aflt')
 
 if __name__ == '__main__':
     pass
--- commands/get_currency.py
+++ commands/get_currency.py
@@ -1,5 +1,13 @@
 from api.moex.price import get_currency_api
+import datetime
 
 
-def get_currency(update, context):
-    currency_answer = update.message.reply_text(get_currency_api())
+def get_all_currency(update, context):
+    currency_array = get_currency_api()
+    print(currency_array['USD/RUB'], currency_array['EUR/RUB'])
+    update.message.reply_text(
+        f'<b>Текущая дата: {datetime.datetime.now().date()}\n'
+        f"Текущая котировка USD/RUB : {currency_array['USD/RUB']}\n"
+        f"Текущая котировка EUR/RUB : {currency_array['EUR/RUB']}</b>\n",
+        parse_mode='HTML'
+    )
