6c9e87d7de5df1709a936f5a540c2e4baa23f74f
igorkorobko <nekto.korobko@gmail.com>
2021-10-24 21:39:24 +0300

    add \n

--- bot/helper.py
+++ bot/helper.py
@@ -6,7 +6,7 @@ def help(update, context):
                               f'/avg50 *ticket*, *ticket - скользящая средняя за 50 дней: /average50 aflt \n'
                               f'/curr - текущая котировка валют: /curr \n'
                               f'/info - информация о торгуемых бумагах биржи: /info \n'
-                              f'/info *ticket* - информация о конкретной бумаге: /info aflt'
+                              f'/info *ticket* - информация о конкретной бумаге: /info aflt \n'
                               f'/alert *ticket* *цена* — Добавить оповещение(SBER — тикер, 354.12 — цена, '
                               f'на которой придет уведомление): /alert SBER 354.12 \n'
                               f'/alert remove *ticket* — Все оповещения по указанному тикету будут удалены: '
