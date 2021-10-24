072b99c7f7aa33316e9dfcf66b9daac43bd8575c
igorkorobko <nekto.korobko@gmail.com>
2021-10-24 21:38:23 +0300

    add help information about /alert, /alert remove and /list

--- bot/helper.py
+++ bot/helper.py
@@ -1,5 +1,3 @@
-
-
 def help(update, context):
     update.message.reply_text(f'/div *ticket* - информация о дивидендах: /div aflt \n'
                               f'/trand *ticket* - тренд эмитента: /trand aflt \n'
@@ -8,7 +6,13 @@ def help(update, context):
                               f'/avg50 *ticket*, *ticket - скользящая средняя за 50 дней: /average50 aflt \n'
                               f'/curr - текущая котировка валют: /curr \n'
                               f'/info - информация о торгуемых бумагах биржи: /info \n'
-                              f'/info *ticket* - информация о конкретной бумаге: /info aflt')
+                              f'/info *ticket* - информация о конкретной бумаге: /info aflt'
+                              f'/alert *ticket* *цена* — Добавить оповещение(SBER — тикер, 354.12 — цена, '
+                              f'на которой придет уведомление): /alert SBER 354.12 \n'
+                              f'/alert remove *ticket* — Все оповещения по указанному тикету будут удалены: '
+                              f'/alert remove SBER \n'
+                              f'/list - показать список всех оповещений: /list')
+
 
 if __name__ == '__main__':
     pass
