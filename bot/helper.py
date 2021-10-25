e18482eb8508ff4c1d9a124e5b66cdbf6fdb23b0
igorkorobko <nekto.korobko@gmail.com>
2021-10-25 03:16:43 +0300

    add new information about commands: alert and subscribe

--- bot/helper.py
+++ bot/helper.py
@@ -1,17 +1,21 @@
 def help(update, context):
-    update.message.reply_text(f'/div *ticket* - информация о дивидендах: /div aflt \n'
-                              f'/trand *ticket* - тренд эмитента: /trand aflt \n'
-                              f'/price *ticket*, *ticket - текущая цена бумаги эмитента: /price aflt \n'
-                              f'/avg15 *ticket*, *ticket - скользящая средняя за 15 дней: /average15 aflt \n'
-                              f'/avg50 *ticket*, *ticket - скользящая средняя за 50 дней: /average50 aflt \n'
-                              f'/curr - текущая котировка валют: /curr \n'
-                              f'/info - информация о торгуемых бумагах биржи: /info \n'
-                              f'/info *ticket* - информация о конкретной бумаге: /info aflt \n'
-                              f'/alert *ticket* *цена* — Добавить оповещение(SBER — тикер, 354.12 — цена, '
-                              f'на которой придет уведомление): /alert SBER 354.12 \n'
-                              f'/alert remove *ticket* — Все оповещения по указанному тикету будут удалены: '
-                              f'/alert remove SBER \n'
-                              f'/list - показать список всех оповещений: /list')
+    update.message.reply_text(
+        f'/div *ticket* - информация о дивидендах: /div aflt \n'
+        f'/trand *ticket* - тренд эмитента: /trand aflt \n'
+        f'/price *ticket*, *ticket - текущая цена бумаги эмитента: /price aflt \n'
+        f'/avg15 *ticket*, *ticket - скользящая средняя за 15 дней: /average15 aflt \n'
+        f'/avg50 *ticket*, *ticket - скользящая средняя за 50 дней: /average50 aflt \n'
+        f'/curr - текущая котировка валют: /curr \n'
+        f'/info - информация о торгуемых бумагах биржи: /info \n'
+        f'/info *ticket* - информация о конкретной бумаге: /info aflt \n'
+        f'/alert *ticket* *цена* — Добавить оповещение(SBER — тикер, 354.12 — цена, '
+        f'на которой придет уведомление): /alert SBER 354.12 \n'
+        f'/alert remove *ticket* — Все оповещения по указанному тикету будут удалены: /alert remove SBER \n'
+        f'/list_alert - показать список всех оповещений: /list_alert'
+        f'/subs *ticket* — Добавить подписку(SBER — тикер)'
+        f'на тикер и тогда по рабочим дням в 09:00(мск) я отправлю сообщение): /subs SBER \n'
+        f'/subs remove *ticket* — Подписка по указанному тикету будут удалена: /subs remove SBER \n'
+        f'/list_alert - показать список всех подписок: /list_subs')
 
 
 if __name__ == '__main__':
