69ad915d28330af64349589eb4f098bbffa319a9
igorkorobko <nekto.korobko@gmail.com>
2021-11-04 01:50:33 +0300

    reformat order functions and optimized function get_date_dividents and get_stock_history. remove unused function get_stock_history_previous_day

--- api/moex/price.py
+++ api/moex/price.py
@@ -32,6 +32,34 @@ def get_working_days():
     return working_days_list
 
 
+def get_previous_working_day():
+    previous_working_day = db_session.query(Calendar.date).filter(Calendar.date < str(datetime.now().date())) \
+        .order_by(desc(Calendar.date)).limit(1).first()[0]
+    return previous_working_day
+
+
+def get_average(emitet, days):
+    all_tickers = get_all_tickers()
+    if emitet in all_tickers:
+        date_ago = (datetime.now() - timedelta(days)).date()
+        yesterday = (datetime.now() - timedelta(1)).date()
+        working_date = db_session.query(Calendar.date).filter(Calendar.date >= str(date_ago),
+                                                              Calendar.date <= str(yesterday)).all()
+        trade_dates = db_session.query(StockHistory.trade_date).filter(StockHistory.sec_id == emitet,
+                                                                       StockHistory.trade_date >= str(date_ago)).all()
+        if len(working_date) != len(trade_dates):
+            stock_history = get_stock_history(emitet, date_ago)
+            if stock_history is None:
+                db_session.close()
+                return None
+        history_close_costs = db_session.query(StockHistory.close_cost, StockHistory.short_name) \
+            .filter(StockHistory.sec_id == emitet, StockHistory.trade_date >= str(date_ago)).all()
+        history_price = [history_close_cost[0] / 100 for history_close_cost in history_close_costs]
+        short_name = db_session.query(StockHistory.short_name).filter(StockHistory.sec_id == emitet).first()[0]
+        db_session.close()
+        return history_price, short_name
+
+
 def get_price(emitet):
     all_tickers = get_all_tickers()
     if emitet in all_tickers:
@@ -69,31 +97,8 @@ def get_price(emitet):
         return stock_info
 
 
-def get_average(emitet, days):
-    all_tickers = get_all_tickers()
-    if emitet in all_tickers:
-        date_ago = (datetime.now() - timedelta(days)).date()
-        yesterday = (datetime.now() - timedelta(1)).date()
-        working_date = db_session.query(Calendar.date).filter(Calendar.date >= str(date_ago),
-                                                              Calendar.date <= str(yesterday)).all()
-        trade_dates = db_session.query(StockHistory.trade_date).filter(StockHistory.sec_id == emitet,
-                                                                       StockHistory.trade_date >= str(date_ago)).all()
-        if len(working_date) != len(trade_dates):
-            stock_history = get_stock_history(emitet, days)
-            if stock_history is None:
-                db_session.close()
-                return None
-        history_close_costs = db_session.query(StockHistory.close_cost, StockHistory.short_name) \
-            .filter(StockHistory.sec_id == emitet, StockHistory.trade_date >= str(date_ago)).all()
-        history_price = [history_close_cost[0] / 100 for history_close_cost in history_close_costs]
-        short_name = db_session.query(StockHistory.short_name).filter(StockHistory.sec_id == emitet).first()[0]
-        db_session.close()
-        return history_price, short_name
-
-
-def get_stock_history(emitet, days):
+def get_stock_history(emitet, date_ago):
     trade_list_dates = []
-    date_ago = (datetime.now() - timedelta(days)).date()
     yesterday = (datetime.now() - timedelta(1)).date()
     url = f'https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/{emitet}.json?'
     parameters = {'from': date_ago, 'till': yesterday}
@@ -101,7 +106,6 @@ def get_stock_history(emitet, days):
     history_data = response['history']['data']
     if len(history_data) != 0:
         for element in history_data:
-            all_data = True
             if element[1] not in trade_list_dates and element[6] is not None:
                 stock_history = StockHistory(
                     board_id=element[0], trade_date=element[1], short_name=element[2], sec_id=element[3],
@@ -118,55 +122,30 @@ def get_stock_history(emitet, days):
             else:
                 return None
         return True
-    else:
-        return None
-
-
-def get_stock_history_previous_day(emitet):
-    previous_working_days = get_previous_working_day()
-    date_now = datetime.now().date()
-    count_date_from_working_days = (date_now - datetime.strptime(previous_working_days, '%Y-%m-%d').date()).days
-    get_history_date = get_stock_history(emitet, count_date_from_working_days)
-    if get_history_date:
-        history_info = db_session.query(
-            StockHistory.short_name,
-            StockHistory.sec_id,
-            StockHistory.open_cost,
-            StockHistory.close_cost,
-            StockHistory.value_trade,
-            StockHistory.number_of_trades,
-            StockHistory.low_cost,
-            StockHistory.high_cost,
-            StockHistory.trade_date).filter(StockHistory.sec_id == emitet,
-                                            StockHistory.trade_date == str(
-                                                previous_working_days)).first()
-        if history_info:
-            return history_info, get_graph(emitet, 15)
 
 
 def get_date_dividents(emitet):
-    dividents_date = db_session.query(Dividents.updated_at).filter(Dividents.sec_id == emitet.upper()).all()
-    len_dividents_date = len(dividents_date)
-    if len_dividents_date and dividents_date == datetime.now().date():
-        dividents_info = db_session.query(Dividents.dividents_data).filter(Dividents.sec_id == emitet.upper()).all()
-        return dividents_info[0][0]
+    dividents_date = db_session.query(Dividents.updated_at).filter(Dividents.sec_id == emitet.upper()).first()
+    if dividents_date and datetime.strftime(dividents_date[0], '%Y-%m-%d') == str(datetime.now().date()):
+        dividents_info = db_session.query(Dividents.dividents_data).filter(Dividents.sec_id == emitet.upper()).first()
+        return dividents_info[0]
     url = f'https://iss.moex.com/iss/securities/{emitet}/dividends.json'
     response = requests.get(url, cookies=authorization.get_auth()).json()
     dividents = response['dividends']['data']
-    if len_dividents_date and dividents:
-        current_info = {'dividents_data': dividents,
-                        'updated_at': datetime.now().date()}
-        db_session.query(Dividents).filter_by(sec_id=emitet).update(current_info)
-        db_session.commit()
-    elif dividents:
-        current_info = Dividents(sec_id=emitet, dividents_data=dividents)
-        db_session.add(current_info)
-        db_session.commit()
-    else:
-        return None
-    dividents_info = db_session.query(Dividents.dividents_data).filter(Dividents.sec_id == emitet.upper())
-    db_session.close()
-    return dividents_info[0][0]
+    if dividents:
+        if dividents_date:
+            current_info = {'dividents_data': dividents,
+                            'updated_at': datetime.now()}
+            db_session.query(Dividents).filter_by(sec_id=emitet).update(current_info)
+            db_session.commit()
+        else:
+            current_info = Dividents(sec_id=emitet, dividents_data=dividents,
+                                     created_at=datetime.now(), updated_at=datetime.now())
+            db_session.add(current_info)
+            db_session.commit()
+        dividents_info = db_session.query(Dividents.dividents_data).filter(Dividents.sec_id == emitet.upper()).first()
+        db_session.close()
+        return dividents_info[0]
 
 
 def get_all_tickers(emitet=''):
@@ -231,7 +210,9 @@ def get_trand(emitet):
             .filter(Trands.sec_id == emitet).first()
         return trand
     else:
-        get_status_history = get_stock_history(emitet, 150)
+        ago_150_days = datetime.now().date() - timedelta(days=150)
+        get_status_history = get_stock_history(emitet, ago_150_days)
+        current_trand_days = 0
         if get_status_history:
             last_100_days_history = db_session.query(StockHistory.close_cost, StockHistory.trade_date) \
                 .order_by(desc(StockHistory.trade_date)) \
@@ -242,18 +223,19 @@ def get_trand(emitet):
             end_15_days = 64
             while start_15_days > -1:
                 is_trand = False
-                history_50_price = [history_trade_date[0]
+                history_price = [history_trade_date[0]
                                     for history_trade_date in last_100_days_history[start_50_days:end_50_days:-1]]
-                history_50_price_sum = round((sum(history_50_price) / 100) / len(history_50_price), 3)
-                history_15_price = [history_trade_date[0]
+                history_50_price_sum = round((sum(history_price) / 100) / len(history_price), 3)
+                history_price = [history_trade_date[0]
                                     for history_trade_date in last_100_days_history[start_15_days:end_15_days:]]
-                history_15_price_sum = round((sum(history_15_price) / 100) / len(history_15_price), 3)
+                history_15_price_sum = round((sum(history_price) / 100) / len(history_price), 3)
                 if history_15_price_sum > history_50_price_sum:
                     is_trand = True
                 trand_info = db_session.query(Trands.current_trand_days, Trands.trand_status) \
                     .filter(Trands.sec_id == emitet).first()
                 if not trand_info:
-                    trand_status = Trands(sec_id=emitet, current_trand_days=1,
+                    current_trand_days = 1
+                    trand_status = Trands(sec_id=emitet, current_trand_days=current_trand_days,
                                           trand_status=is_trand,
                                           trand_date=datetime.now().date(),
                                           created_at=datetime.now(),
@@ -263,11 +245,12 @@ def get_trand(emitet):
                     db_session.add(trand_status)
                     db_session.commit()
                 else:
-                    current_trand_days = 1
                     if trand_info[1] == is_trand:
-                        current_trand_days = trand_info[0] + 1
+                        current_trand_days += 1
+                    else:
+                        current_trand_days = 0
                     trand_status = {'sec_id': emitet,
-                                    'current_trand_days': current_trand_days,
+                                    # 'current_trand_days': current_trand_days,
                                     'trand_status': is_trand,
                                     'trand_date': datetime.now().date(),
                                     'updated_at': datetime.now(),
@@ -279,19 +262,16 @@ def get_trand(emitet):
                 end_50_days -= 1
                 start_15_days -= 1
                 end_15_days -= 1
-    trand = db_session.query(
-        Trands.sec_id,
-        Trands.trand_status,
-        Trands.current_trand_days,
-        Trands.average_15, Trands.average_50) \
-        .filter(Trands.sec_id == emitet).first()
-    return trand
-
-
-def get_previous_working_day():
-    previous_working_day = db_session.query(Calendar.date).filter(Calendar.date < str(datetime.now().date())) \
-        .order_by(desc(Calendar.date)).limit(1).first()[0]
-    return previous_working_day
+        trand_days = {'current_trand_days': current_trand_days}
+        db_session.query(Trands).filter_by(sec_id=emitet).update(trand_days)
+        db_session.commit()
+        trand = db_session.query(
+            Trands.sec_id,
+            Trands.trand_status,
+            Trands.current_trand_days,
+            Trands.average_15, Trands.average_50) \
+            .filter(Trands.sec_id == emitet).first()
+        return trand
 
 
 if __name__ == "__main__":
